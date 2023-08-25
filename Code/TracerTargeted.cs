b3512ae74c249f86983c4e1fb0dba84c2a96b0c9
EchKode <48565771+echkode@users.noreply.github.com>
2023-08-25 13:35:36 +0000

    Create a subsystem function for tracer rounds

---
+++ Code/TracerTargeted.cs
@@ -0,0 +1,57 @@
+﻿using PhantomBrigade.Data;
+using PhantomBrigade.Functions;
+
+using UnityEngine;
+
+namespace EchKode.PBMods.BurstFire
+{
+	public class TracerTargeted : ISubsystemFunctionTargeted
+	{
+		public int modulus;
+		public int startingOffset;
+		public DataBlockColorInterpolated color;
+
+		public void OnPartEventTargeted(
+			EquipmentEntity subsystem,
+			string context,
+			Vector3 position,
+			Vector3 direction,
+			Vector3 targetPosition,
+			CombatEntity targetUnitCombat,
+			CombatEntity projectile)
+		{
+			if (context != PartEventsTargeted.OnWeaponFired)
+			{
+				return;
+			}
+
+			if (color == null)
+			{
+				return;
+			}
+
+			if (!projectile.hasAssetLink)
+			{
+				return;
+			}
+
+			if (projectile.assetLink.instance == null)
+			{
+				return;
+			}
+
+			if (!projectile.hasProjectileIndex)
+			{
+				return;
+			}
+
+			var isTracer = (projectile.projectileIndex.i + startingOffset) % modulus == 0;
+			if (!isTracer)
+			{
+				return;
+			}
+
+			projectile.assetLink.instance.UpdateColors(null, color);
+		}
+	}
+}
